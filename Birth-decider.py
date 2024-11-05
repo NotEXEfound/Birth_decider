95a42df640fb5153cd1384e6146a02b0a38f956a
NotEXEfound <nadderimonkey@gmail.com>
2024-11-05 16:30:15 +1100

    Add files via upload

---
+++ Birth-decider.py
@@ -0,0 +1,11 @@
+year = 2024
+
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+age = int(input("Please enter your age: "))
+
+print(f"{first_name} {last_name} (Birth Year: {year - age})")
+
+# Keep the program running indefinitely
+while True:
+    pass
